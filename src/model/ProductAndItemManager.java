d4c4620bf2dfa078a65804eeb2a2bb011b6f3f98
Evan Manning <evan@ManningLaptop.app.byu.edu>
2013-07-03 22:36:02 -0600

    Finished stubs of ProductAndItemEditor

--- src/model/ProductAndItemManager.java
+++ src/model/ProductAndItemManager.java
@@ -1,26 +1,14 @@
 package model;
 
+import java.util.Date;
+
 public class ProductAndItemManager {
-    
-    /*
-    + addItem(Item): void
-    + removeItem(Item): void
-    + moveItem(Item, Container): void
-    + editItem(Item, Item): void
-    */
     
     /** Adds a product to the system
      * @pre     product must not already exist in the set
      * @post    product is added to the set
      */
     public void addProduct(Product product) {}
-    
-    /** Deletes a Product from the set all of products.
-     * @pre     product must exist in the set
-     * @pre     product must not have any Items attached to it
-     * @post    removes the Product from the set
-     */
-    public void deleteProduct(Product product) {}
     
     /** Edits a Product by updating by replacing an older Product with a newer Product.
      * @pre     oldProduct must exist in the set of products
@@ -35,5 +23,62 @@ public class ProductAndItemManager {
      * @param       after   the new container that the product is being move to
      */
     public void moveProduct(Product product, Container before, Container after) {}
+    
+    /** Deletes a Product from the set all of products.
+     * @pre     product must exist in the set
+     * @pre     product must not have any Items attached to it
+     * @post    removes the Product from the set
+     */
+    public void deleteProduct(Product product) {}
+
+    /**
+     * 
+     * @pre canAddItem() == true
+     * @post itemToAdd.container = container, itemToAdd.prodcut = product, itemToAdd.expirationDate = expirationDate
+     * 
+     * @throws IllegalArgumentException if !canAddItem()
+     */
+     public void addItem(Item itemToAdd, Date expirationDate) throws IllegalArgumentException {
+     
+     }
+    
+    /**
+      * 
+      * @param oldItem - item before edit
+      * @param newItem - item after edit
+      * 
+      * @throws IllegalArgumentException
+      */
+     public void editItem(Item oldItem, Item newItem) throws IllegalArgumentException {
+    
+     }
+
+    /**
+     * Updates indexes for the move.
+     * 
+     * @pre itemToMove.product exists (only) in the target container
+     * @post itemToMove.container = target, itemToMove.storageUnit = target's storage unit
+     * 
+     * @throws IllegalStateException if pre-conditions are not met
+     * @throws IllegalArgumentExcpetion if itemToMove is bad
+     */
+     public void moveItem(Item itemToMove, Container target) throws IllegalStateException, IllegalArgumentException {
+     
+     }
+     
+     /** Removes the item from any container but keeps track of it in the item history.
+      * 
+      * @param itemToRemove - item to be removed
+      * 
+      * @pre itemToRemove.container != null, !removedItems.contains(itemToMove), itemToMove.exitTime == null
+      * @post captures and sets itemToRemove.exitTime, sets itemToRemove.container to null, adds to removedItems, updates indexItemsByRemovalDate
+      * 
+      * @throws IllegalStateException if preconditions are not met
+      * @throws IllegalArgumentException if itemToRemove is bad
+      * 
+      */
+     public void removeItem(Item itemToRemove) throws IllegalStateException, IllegalArgumentException {
+     
+     }
 
 }
