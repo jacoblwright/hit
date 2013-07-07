d88d57839203df597cfb8f893a7914a686b324b2
Evan Manning <evan@ManningLaptop-2.local>
2013-07-06 22:58:33 -0600

    Worked on impl of editors, Serializer, Controller; also added Apache commons IO library.

--- src/model/ProductAndItemEditor.java
+++ src/model/ProductAndItemEditor.java
@@ -8,9 +8,11 @@ public class ProductAndItemEditor {
     private ItemManager itemManager;
     
     /**
-     * 
-     * @param productManager
-     * @param itemManager
+     * Constructs the editor with the specified managers.
+     * @param productManager the ProductManager that this editor will use
+     * @param itemManager the ItemManager that this editor will use
+     * @pre productManager and itemManager must not be null.
+     * @post This editor has been initialized using the specified managers.
      */
     public ProductAndItemEditor(ProductManager productManager,
             ItemManager itemManager) {
@@ -25,73 +27,99 @@ public class ProductAndItemEditor {
     
     /**
      * Adds a product to the system.
-     * @param   product the product to be added
-     * @pre     product must not already exist in the system.
-     * @post    product has been added to the system.
+     * @param product the product to be added
+     * @pre product must not already exist in the system.
+     * @post product has been added to the system.
      */
-    public void addProduct(Product product) {}
+    public void addProduct(Product product) {
+        
+        productManager.addProduct(product);
+        
+    }
     
-    /** Edits a Product by updating by replacing an older Product with a newer Product.
-     * @pre     oldProduct must exist in the set of products
-     * @post    replaces the oldProduct with the new Product
+    /**
+     * Edits a Product by replacing an older Product with a newer Product.
+     * @pre oldProduct must exist in the system.
+     * @post oldProduct has been replaced with the newProduct.
      */
-    public void editProduct(Product newProduct, Product oldProduct) {}
+    public void editProduct(Product oldProduct, Product newProduct) {
+        
+        productManager.editProduct(oldProduct, newProduct);
+        
+    }
     
-    /** Moves a product to a new container as long as that product is not already located in that storage unit.
-     * @pre         product is not already in the container it is being moved into
-     * @param       product the product being moved
-     * @param       before  the container before the product was moved
-     * @param       after   the new container that the product is being move to
+    /**
+     * Moves a product to a new container as long as that product is not
+     * already located in that storage unit.
+     * @pre product is not already in the container it is being moved into.
+     * @param product the product being moved
+     * @param before the container before the product was moved
+     * @param after the new container that the product is being moved to
      */
-    public void moveProduct(Product product, Container before, Container after) {}
+    public void moveProduct(Product product,
+            Container before, Container after) {
+        
+        productManager.moveProduct(product, before, after);
+        
+    }
     
     /** Deletes a Product from the set all of products.
-     * @pre     product must exist in the set
-     * @pre     product must not have any Items attached to it
-     * @post    removes the Product from the set
+     * @pre product must exist in the set
+     * @pre product must not have any Items attached to it
+     * @post removes the Product from the set
      */
-    public void deleteProduct(Product product) {}
+    public void deleteProduct(Product product) {
+        
+        productManager.deleteProduct(product);
+        
+    }
 
     /**
-     * 
      * @pre canAddItem() == true
-     * @post itemToAdd.container = container, itemToAdd.prodcut = product, itemToAdd.expirationDate = expirationDate
-     * 
+     * @post itemToAdd.container = container, itemToAdd.prodcut = product,
+     * itemToAdd.expirationDate = expirationDate
      */
      public void addItem(Item itemToAdd, Date expirationDate) {
      
+         itemManager.addItem(itemToAdd, expirationDate);
+         
      }
     
     /**
-      * 
       * @param oldItem - item before edit
-      * @param newItem - item after edit
-      * 
+      * @param newItem - item after edit 
       */
      public void editItem(Item oldItem, Item newItem) {
     
+         itemManager.editItem(oldItem, newItem);
+         
      }
 
     /**
      * Updates indexes for the move.
-     * 
      * @pre itemToMove.product exists (only) in the target container
      * @post itemToMove.container = target, itemToMove.storageUnit = target's storage unit
      */
      public void moveItem(Item itemToMove, Container target) {
      
+         itemManager.moveItem(itemToMove, target);
+         
      }
      
-     /** Removes the item from any container but keeps track of it in the item history.
-      * 
-      * @param itemToRemove - item to be removed
-      * 
-      * @pre itemToRemove.container != null, !removedItems.contains(itemToMove), itemToMove.exitTime == null
-      * @post captures and sets itemToRemove.exitTime, sets itemToRemove.container to null, adds to removedItems, updates indexItemsByRemovalDate
-      * 
+     /**
+      * Removes the item from any container but keeps track of it in the item
+      * history.
+      * @param itemToRemove - item to be removed.
+      * @pre itemToRemove.container != null,
+      * !removedItems.contains(itemToMove), itemToMove.exitTime == null
+      * @post captures and sets itemToRemove.exitTime,
+      * sets itemToRemove.container to null, adds to removedItems,
+      * updates indexItemsByRemovalDate
       */
      public void removeItem(Item itemToRemove) {
      
+         itemManager.removeItem(itemToRemove);
+         
      }
 
 }
--- src/model/Serializer.java
+++ src/model/Serializer.java
@@ -1,62 +1,80 @@
 package model;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import org.apache.commons.io.*;
 
-/**Serializes and Deserializes objects
- * 
+/**
+ * Serializes and deserializes objects
  * @author jake
- *
  */
 public class Serializer {
 	
-	/**Serializes an object to a byte[]
-	 * @pre	none
-	 * @param obj
-	 * @return byte[]
-	 * @throws IOException
-	 */
-	 private static byte[] serialize(Object obj) throws IOException {
-	       ByteArrayOutputStream b = new ByteArrayOutputStream();
-	       ObjectOutputStream o = new ObjectOutputStream(b);
-	       o.writeObject(obj);
-	       return b.toByteArray();
-	  }
+	/**
+	 * Given a file it writes the state of the core object model there
+      * @pre valid file
+      * @post saveState(file)
+      * @throws IOException
+      */
+     public static void save(Object object, File file) throws IOException {
+         
+         OutputStream os = new FileOutputStream(file);
+    	 IOUtils.write(serialize(object), os);
+    	 os.close();
+         
+     }
 
-	 /**Deserializes a byte[] to an object
-	  * @pre nones
-	  * @param bytes
-	  * @return	object
-	  * @throws IOException
-	  * @throws ClassNotFoundException
-	  */
-     private static Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+     /**
+      * Given a file it reads it and loads the state of the core object model
+      * @pre valid file
+      * @post loadState(file)
+      * @throws IOException
+     * @throws ClassNotFoundException 
+      */
+     public static Object load(File file)
+             throws IOException, ClassNotFoundException {
+    	 
+         InputStream is = new FileInputStream(file);
+         Object object = deserialize(IOUtils.toByteArray(is));
+         is.close();
+         
+         return object;
+         
+     }
+     
+     /**
+     * Serializes an object to a byte[]
+     * @pre	none
+     * @param obj
+     * @return byte[]
+     * @throws IOException
+     */
+     private static byte[] serialize(Object obj) throws IOException {
+         
+         assert true;
+         
+         ByteArrayOutputStream b = new ByteArrayOutputStream();
+         ObjectOutputStream o = new ObjectOutputStream(b);
+         o.writeObject(obj);
+         return b.toByteArray();
+    
+     }
+
+    /**Deserializes a byte[] to an object
+      * @pre none
+      * @param bytes
+      * @return	object
+      * @throws IOException
+      * @throws ClassNotFoundException
+      */
+     private static Object deserialize(byte[] bytes)
+             throws IOException, ClassNotFoundException {
+         
+         assert true;
+         
          ByteArrayInputStream b = new ByteArrayInputStream(bytes);
          ObjectInputStream o = new ObjectInputStream(b);
          return o.readObject();
+    
      }
-     
-     /** Given a file it reads it and loads the state of the core object model
-      * @pre						valid file
-      * @post						loadState(file)
-      * @throws IOException
-      */
-     public void load( File file) throws IOException {
-    	 
-     }
-     
-     /** Given a file it writes the state of the core object model there
-      * @pre						valid file
-      * @post						saveState(file)
-      * @throws IOException
-      */
-     public void save( File file ) throws IOException {
-    	 
-     }
-     
-     
+       
 }
