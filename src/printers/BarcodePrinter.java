4a44c1aa0a9a15d35c67c5abd1bc7c61d94d1cb2
Evan Manning <evan@ManningLaptop-2.local>
2013-07-13 23:41:01 -0600

    BarcodePrinter essentially complete.

--- src/printers/BarcodePrinter.java
+++ src/printers/BarcodePrinter.java
@@ -1,14 +1,22 @@
 package printers;
 
 import java.util.*;
+import java.util.List;
 import java.io.*;
+
 import model.*;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.*;
+import com.itextpdf.text.pdf.Barcode;
 
 /**
  * Provides a method to create a PDF file with images of barcodes for a
  * specified List of Item objects.
  */
 public class BarcodePrinter {
+    
+    private final int MARGINS_PT = 36;
+    private final int NUM_OF_COLUMNS = 3;
     
     /**
      * Creates an instance of BarcodePrinter.
@@ -30,13 +38,114 @@ public class BarcodePrinter {
      * file is not null and it is writable.
      * @throws IllegalArgumentException if items or any of its contained
      * Item objects are null, or if file is null.
-     * @throws IOException if file is not writable or if there is a problem
-     * writing to the file.
+     * @throws IOException if file is a directory, not writable, or if there
+     * is a problem writing to the file.
      */
     public void printBarcodes(List<Item> items, File file)
         throws IllegalArgumentException, IOException {
         
+        checkItems(items);
         
+        checkFile(file);
+        
+        try {           
+            
+            Document document = new Document(PageSize.LETTER);
+            
+            PdfWriter writer = PdfWriter.getInstance(
+                    document, new FileOutputStream(file));
+                        
+            document.open();            
+            document.setMargins(
+                    MARGINS_PT, MARGINS_PT, MARGINS_PT, MARGINS_PT);
+            
+            PdfContentByte pcb = writer.getDirectContent();
+            
+            PdfPTable table = new PdfPTable(NUM_OF_COLUMNS);
+            table.setWidthPercentage(100f);
+            
+            for (int i = 0; i < items.size(); i++) {
+                
+                Item item = items.get(i);
+                
+                Phrase phr = new Phrase();
+                phr.add(item.getProduct().getDescription() + "\n");
+                phr.add("Ent: " + item.getEntryDate() + "\n");
+                phr.add("Exp: " + item.getExpirationDate() + "\n");
+                phr.add("\n");
+                
+                BarcodeEAN codeEAN = new BarcodeEAN();
+                codeEAN.setCodeType(Barcode.UPCA);
+                codeEAN.setCode(item.getTag().getBarcode());   
+                Image barcode =
+                        codeEAN.createImageWithBarcode(pcb, null, null);
+                
+                phr.add(new Chunk(barcode, 0f, 0f));
+                
+                PdfPCell cell = new PdfPCell(phr);
+                cell.setFixedHeight(144f);
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                
+                table.addCell(cell);
+            
+            }
+            
+            int remainder = items.size() % NUM_OF_COLUMNS;
+            //System.out.println(remainder);
+            if (remainder != 0) {
+                
+                for (int i = 0; i < NUM_OF_COLUMNS - remainder; i++) {
+                    table.addCell(new PdfPCell());
+                }
+                
+            }
+            
+            document.add(table);
+            
+            document.close();
+        
+        }
+        catch (DocumentException e) {
+            throw new IOException(e.getMessage());
+        }
+        
+    }
+    
+    private void checkItems(List<Item> items) {
+        
+        if (items == null) {            
+            throw new IllegalArgumentException("Specified Item List is null.");           
+        }
+        
+        for (Item i : items) {
+            if (i == null) {
+                throw new IllegalArgumentException(
+                        "Specified Item List containes null element(s).");
+            }
+        }
+
+    }
+    
+    private void checkFile(File file) throws IOException {
+        
+        System.out.println(file);
+        
+        if (file == null) {
+            throw new IllegalArgumentException("Specified file is null.");
+        }
+        
+        if (file.exists() && (!file.isFile() || !file.canWrite())) {
+            throw new IOException("Specified file is not valid.");
+        }
+        if (!file.exists()) {
+            
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+            
+            file.createNewFile();
+            
+        }
         
     }
 
