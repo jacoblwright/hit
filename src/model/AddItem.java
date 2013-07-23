feefdfaf35426494f8eff65ff64bf3eb19950c25
Stockholm <andrew@ubuntu.(none)>
2013-07-23 09:33:35 -0700

    Added new classes undo/redo functionality

---
+++ src/model/AddItem.java
@@ -0,0 +1,26 @@
+package model;
+
+/** A sub command that adds an item to the system.
+ * 
+ * @author andrew
+ *
+ */
+public class AddItem implements Command{
+	
+	/** A constructor that sets the item being added to the system.
+	 * 
+	 * @param item	the Item being added to the system.
+	 */
+	public AddItem(Item item){}
+	
+	/** Adds item to the system
+	 * 
+	 */
+	public void execute(){}
+	
+	/** Removes item from the system
+	 * 
+	 */
+	public void unexecute(){}
+
+}
---
+++ src/model/AddNewProduct.java
@@ -0,0 +1,28 @@
+package model;
+
+/** A sub command that adds a product to a particular container.
+ * 
+ * @author andrew
+ *
+ */
+public class AddNewProduct implements Command{
+	
+	/** A constructor that sets the Product being added and sets the Container product
+	 * is being added to.
+	 * 
+	 * @param product	the Product being added to container
+	 * @param container		the Container to which product is being added
+	 */
+	public AddNewProduct(Product product, Container container){}
+	
+	/** Adds new Product to the system in the designated Container as set in the constructor
+	 * 
+	 */
+	public void execute(){}
+	
+	/** Removes a Product from the system and detaches it from the container
+	 * 
+	 */
+	public void unexecute(){}
+
+}
