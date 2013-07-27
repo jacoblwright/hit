d8d80eb7910a36ef64ca2acd02a412e81c4973d6
Evan Manning <evan@ManningLaptop-2.local>
2013-07-26 23:12:52 -0600

    prep

--- src/reports/ReportDirector.java
+++ src/reports/ReportDirector.java
@@ -2,12 +2,14 @@ package reports;
 
 import java.util.*;
 import java.io.*;
+import model.*;
 import config.IOConfig;
 import printers.*;
+import gui.common.*;
 
 public class ReportDirector {
     
-    public static void generateExpiredItemsReport(PrintFormat format) 
+    public static void generateExpiredItemsReport(FileFormat format) 
             throws IOException {
         
         ReportPrinter p = getPrinter(format);
@@ -15,6 +17,7 @@ public class ReportDirector {
         p.printTitle("Expired Items Report");
         p.printBlankLine();
         p.printBlankLine();
+        
         List<String> columnHeadings = new LinkedList<String>();
         columnHeadings.add("Description");
         columnHeadings.add("Storage Unit");
@@ -22,36 +25,50 @@ public class ReportDirector {
         columnHeadings.add("Entry Date");
         columnHeadings.add("Expriation Date");
         columnHeadings.add("Item Barcode");
+        
+        Set<Container> containerRoot = new HashSet<Container>();
+        containerRoot.addAll(
+                Model.getInstance().getContainerManager().getRoot());
+        Iterator<Container> itr = new ContainerPreorderIterator(containerRoot);
+        Visitor visitor = new ExpiredItemsVisitor(itr);
+        List<Record> records = visitor.visitAll();
+        List<List<String>> recordsAsStrings = new LinkedList<List<String>>();
+        for (Record record : records) {
+            recordsAsStrings.add(record.getValuesAsStrings());
+        }
+        p.printTable(columnHeadings, recordsAsStrings);
+        
+        p.close(true);
 
     }
 
     public static void generateRemovedItemsReport(Date startDate,
-            PrintFormat format) {
+            FileFormat format) {
         
         
         
     }
 
     public static void generateNMonthSupplyReport(int numOfMonths,
-            PrintFormat format) {
+            FileFormat format) {
     
         
     
     }
 
     public static void generateProductStatReport(Date startDate, Date endDate,
-            PrintFormat format) {
+            FileFormat format) {
     
         
     
     }
     
-    public static void generatNoticesReport(PrintFormat format) {
+    public static void generatNoticesReport(FileFormat format) {
         
         
     }
     
-    private static ReportPrinter getPrinter(PrintFormat format)
+    private static ReportPrinter getPrinter(FileFormat format)
             throws IOException {
         
         ReportPrinter p = null;
