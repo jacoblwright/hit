ed577f4bde09852dd22a7539f2ce250a95b6c3f1
Jacob Wright <jake.wright09@hotmail.com>
2013-08-03 18:29:41 -0600

    added plugins

--- plugins/AmazonAutoIdentityPlugin.java
+++
@@ -1,9 +0,0 @@
-@PluginImplementation
-public class AmazonAutoIdentityPlugin implements AutoIdentityPlugin {
-
-	public String getDescription(String upc) {
-		// TODO Auto-generated method stub
-		return "Hello World";
-	}
-
-}
