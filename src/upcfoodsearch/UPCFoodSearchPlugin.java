ee114930558dab5a8321e2e8e31212dd3c5dccbd
Stockholm <andrew@ubuntu.(none)>
2013-08-05 19:26:38 -0700

    Added Food Search Plugin

---
+++ src/upcfoodsearch/UPCFoodSearchPlugin.java
@@ -0,0 +1,62 @@
+package upcfoodsearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+
+@PluginImplementation
+public class UPCFoodSearchPlugin implements AutoIdentityPlugin{
+
+	@Override
+	public String getDescription(String upc) {
+		
+		String descr = null;
+		String url = "http://www.upcfoodsearch.com/upc-";
+		try {
+			URL urlin = new URL(url + upc);
+			HttpURLConnection con = (HttpURLConnection) urlin.openConnection();
+			con.setRequestMethod("GET");
+			int responseCode = con.getResponseCode();
+	 
+			BufferedReader br = new BufferedReader(
+			        new InputStreamReader(con.getInputStream()));
+			String buffer;
+			String page = "";
+			while ((buffer = br.readLine()) != null) {
+				page += buffer;
+			}
+			br.close();
+			
+			return parseDescription(page);
+		}
+		
+		catch (MalformedURLException e) {
+				descr = null;
+		} 
+		catch (IOException e) {
+				descr = null;
+		}
+		return null;
+	}
+	
+	public String parseDescription(String page){
+		String regexString = "<div class=\"blockTitleL cen\"><h1>(.*?)</h1></div>";
+		Pattern pattern = Pattern.compile(regexString);
+		Matcher matcher = pattern.matcher(page);
+
+		if (matcher.find()) {
+		  if(matcher.group(1).equals("Not Found"))
+				  return null;
+		  else return matcher.group(1);
+		}
+		else return null;
+	}
+
+}
---
+++ test-src/plugins/UPCFoodSearchPluginTest.java
@@ -0,0 +1,17 @@
+package plugins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import upcfoodsearch.UPCFoodSearchPlugin;
+
+public class UPCFoodSearchPluginTest {
+
+	@Test
+	public void getDescriptionTest() {
+		UPCFoodSearchPlugin search = new UPCFoodSearchPlugin();
+		System.out.println(search.getDescription("023896177007"));	
+	}
+
+}
